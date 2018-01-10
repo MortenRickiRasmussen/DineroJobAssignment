76b32ed7398ac7b58750a41d485570adbcc8e007
Morten Ricki Rasmussen <MortyHD@live.dk>
2018-01-10 18:22:06 +0100

    Add all

--- Program.cs
+++ Program.cs
@@ -2,35 +2,52 @@
 
 namespace dinero
 {
+    [Flags]
+    enum TriangleType : int
+    {
+        equilateral,
+        isoseles,
+        scalene    
+    }
+
     class Program
     {
+
+        public static int getTriangleType(int a, int b, int c) {
+
+            if (a == b && b == c) {
+                return (int) TriangleType.equilateral;
+            } 
+            
+            if (a == b || a == c || b == c) {
+                return (int) TriangleType.isoseles;
+            } 
+
+            return (int) TriangleType.scalene;
+        }
+
         static void Main(string[] args)
         {
-
-            const string EQUILATERAL = "equilateral";
-            const string ISOSELES = "isoseles";
-            const string SCALENE = "scalene";
-
-            string result;
-
             Start:
 
             Console.WriteLine("Please enter the triangles three sides, seperated by \"Enter\"");
+
             var sideA = Console.ReadLine();
             var sideB = Console.ReadLine();
             var sideC = Console.ReadLine();
-            Console.WriteLine("Thank you");
-
-            if (sideA == sideB && sideB == sideC) {
-                result = EQUILATERAL;
-            } else if (sideA == sideB || sideA == sideC || sideB == sideC) {
-                result = ISOSELES;
-            } else {
-                result = SCALENE;
+            
+            if (!int.TryParse(sideA, out var a ) || !int.TryParse(sideB, out var b) || !int.TryParse(sideC, out var c)) {
+                Console.WriteLine("One of the entered values are not an interger");
+                goto Start;
             }
+
+            int result = getTriangleType(a, b, c);
+
+
             Console.WriteLine("You triangle is " + result);
 
             goto Start;
+
         }
     }
 }
