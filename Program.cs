558a554cf8336a2d6957a16d81636b5b181d855a
Morten Ricki Rasmussen <MortyHD@live.dk>
2018-01-10 17:52:27 +0100

    First Version

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dinero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string EQUILATERAL = "equilateral";
+            const string ISOSELES = "isoseles";
+            const string SCALENE = "scalene";
+
+            string result;
+
+            Start:
+
+            Console.WriteLine("Please enter the triangles three sides, seperated by \"Enter\"");
+            var sideA = Console.ReadLine();
+            var sideB = Console.ReadLine();
+            var sideC = Console.ReadLine();
+            Console.WriteLine("Thank you");
+
+            if (sideA == sideB && sideB == sideC) {
+                result = EQUILATERAL;
+            } else if (sideA == sideB || sideA == sideC || sideB == sideC) {
+                result = ISOSELES;
+            } else {
+                result = SCALENE;
+            }
+            Console.WriteLine("You triangle is " + result);
+
+            goto Start;
+        }
+    }
+}
